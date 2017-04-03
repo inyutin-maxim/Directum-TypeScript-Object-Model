117c96088d6f1b6bba18a873284c884cee242d39
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2017-04-03 18:16:12 +0300

    добавил todo
    Signed-off-by: Инютин Максим Николаевич <inyutin_maxim@mail.ru>

--- Directum/Data/IDataSet.ts
+++ Directum/Data/IDataSet.ts
@@ -1,7 +1,42 @@
 import {IQuery} from "./IQuery";
 /**
  * Объект IDataSet предоставляет методы и свойства для работы с набором данных.
+ *
+ * todo Описать интерфейс
  */
 export interface IDataSet extends IQuery{
+    readonly AccessInfo: string;
+    readonly ActualRecordCount: string;
+    readonly AsQuery: string;
+    readonly Bookmark: string;
+    readonly Component: string;
+    readonly Copied: string;
+    readonly Events: string;
+    readonly Inserted: string;
+    readonly MainDataSet: string;
+    readonly Modified: string;
+    readonly PreviousValuesStored: string;
+    readonly RecordOpened: string;
+    readonly RequisiteCount: string;
+    readonly RequisiteFactory: string;
+    readonly Requisites: string;
+    readonly SQLTableName: string;
+    readonly State: string;
+    readonly TableName: string;
 
+    Append(): void;
+    ApplyUpdates(): void;
+    AssignRecord(): void;
+    CancelUpdates(): void;
+    CloseRecord(): void;
+    Delete(): void;
+    DetailDataSet(): void;
+    DetailExists(): void;
+    DisableControls(): void;
+    EnableControls(): void;
+    FindRequisite(): void;
+    Insert(): void;
+    OpenRecord(): void;
+    Refresh(): void;
+    RequisiteByIndex(): void;
 }
