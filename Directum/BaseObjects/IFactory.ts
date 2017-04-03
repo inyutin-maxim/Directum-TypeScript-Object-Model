48f12dfecdc6ae961f7f44f2da3f55197b267ebd
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2017-04-03 17:53:45 +0300

    IFactory.ts
    Signed-off-by: Инютин Максим Николаевич <inyutin_maxim@mail.ru>

--- Directum/BaseObjects/IFactory.ts
+++ Directum/BaseObjects/IFactory.ts
@@ -1,8 +1,30 @@
+import {IObject} from "../IObject";
+import {IApplication} from "../IApplication";
+import {TContentKind} from "../Enums/TContentKind";
+import {IObjectInfo} from "./IObjectInfo";
 /**
  * Фабрики предназначены для получения объектов системы.
- *
- * todo описать интерфейс
  */
 export interface IFactory {
+    /**
+     * Свойство возвращает объект-приложение, которому принадлежит фабрика.
+     */
+    readonly Application: IApplication;
+    /**
+     * Свойство возвращает тип объектов, к которым предоставляет доступ фабрика.
+     */
+    readonly Kind: TContentKind;
+    /**
+     * Свойство предоставляет информацию об объекте по его ИД.
+     *
+     * @param id ИД объекта.
+     */
+    ObjectInfo(id: number): IObjectInfo;
 
+    /**
+     * Метод возвращает объект по его ИД.
+     *
+     * @param id ИД объекта.
+     */
+    GetObjectByID(id: number): IObject;
 }
