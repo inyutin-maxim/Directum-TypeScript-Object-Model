1ebf99d8ab1b1cbf1ba5bbddf02e63b18120b670
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2017-04-04 11:16:31 +0300

    Описал IStringList.ts
    Signed-off-by: Инютин Максим Николаевич <inyutin_maxim@mail.ru>

--- Directum/AuxiliaryObjects/IStringList.ts
+++ Directum/AuxiliaryObjects/IStringList.ts
@@ -1,21 +1,69 @@
 import {IForEach} from "./IForEach";
+import {TStringsSortType} from "../Enums/TStringsSortType";
 /**
  * Объект IStringList предназначен для хранения набора строк.
- *
- * todo описать интерфейс
  */
 export interface IStringList extends IForEach {
-    readonly DelimitedText: string;
-    readonly Delimiter: string;
-    readonly Sorted: string;
-    readonly SortType: string;
-    readonly Text: string;
-    readonly Values: string;
-
-    Add(): void;
-    AddStrings(): void;
+    /**
+     * Свойство предоставляет доступ к списку строк в виде единой строки с разделителями.
+     */
+    DelimitedText: string;
+    /**
+     * Свойство предоставляет доступ к разделителю строк списка.
+     */
+    Delimiter: string;
+    /**
+     * Свойство предоставляет доступ к признаку сортировки списка.
+     */
+    Sorted: boolean;
+    /**
+     * Свойство предоставляет доступ к типу сортировки строк.
+     */
+    SortType: TStringsSortType;
+    /**
+     * Свойство предоставляет доступ к списку строк в виде текста, в котором строки разделены парой символов:
+     * возврата каретки (#13) и перевода строки (#10).
+     */
+    Text: string;
+    /**
+     * Свойство возвращает строку списка с указанным индексом.
+     *
+     * @param index индекс строки в списке.
+     */
+    Values(index: number): string;
+    /**
+     * Метод добавляет в список новую строку.
+     *
+     * @param value строка.
+     */
+    Add(value: string): number;
+    /**
+     * Метод добавляет к списку новые строки из параметра Source.
+     *
+     * @param source список строк.
+     */
+    AddStrings(source: IStringList): void;
+    /**
+     * Метод удаляет все строки списка. После удаления значение свойства Count становится равным нулю.
+     */
     Clear(): void;
-    Delete(): void;
-    Exchange(): void;
-    IndexOf(): void;
+    /**
+     * Метод удаляет строку с указанным индексом.
+     *
+     * @param index индекс удаляемой строки.
+     */
+    Delete(index: number): void;
+    /**
+     * Метод меняет местами 2 строки в списке.
+     *
+     * @param index1 индекс первой строки
+     * @param index2 индекс второй строки
+     */
+    Exchange(index1: number, index2: number): void;
+    /**
+     * Метод возвращает индекс строки, совпадающей со значением параметра Value.
+     *
+     * @param value искомая строка
+     */
+    IndexOf(value: string): number;
 }
