3f1caf8cc144d739ec6c806e4e2b2c3a143c5558
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2017-04-04 11:20:32 +0300

    Описал IScriptFactory.ts
    Signed-off-by: Инютин Максим Николаевич <inyutin_maxim@mail.ru>

--- Directum/Factories/IScriptFactory.ts
+++ Directum/Factories/IScriptFactory.ts
@@ -1,4 +1,20 @@
 import {IScript} from "../IScript";
-export interface IScriptFactory{
+import {IFactory} from "../BaseObjects/IFactory";
+/**
+ * Объект IScriptFactory предназначен для работы со сценариями.
+ */
+export interface IScriptFactory extends IFactory {
+    /**
+     * Метод выполняет сценарий с именем Name.
+     *
+     * @param name имя сценария
+     */
+    ExecuteByName(name: string): any;
+    /**
+     * Метод возвращает объект-сценарий IScript.
+     * Если сценария с именем Name не существует, то будет сгенерировано исключение.
+     *
+     * @param name имя сценария
+     */
     GetObjectByName(name: string) : IScript;
 }
