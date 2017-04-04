76584505e79cad7beb6de7b8a3fedf213529d3a2
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2017-04-04 12:36:41 +0300

    Описал IRuleList.ts
    Signed-off-by: Инютин Максим Николаевич <inyutin_maxim@mail.ru>

--- Directum/References/IRuleList.ts
+++ Directum/References/IRuleList.ts
@@ -1,8 +1,23 @@
+import {ICheckPointedList} from "./ICheckPointedList";
+import {IList} from "../AuxiliaryObjects/IList";
+import {IForEach} from "../AuxiliaryObjects/IForEach";
+import {IRule} from "./IRule";
 /**
  * Объект IRuleList предназначен для хранения набора правил.
- *
- * todo описать интерфейс
  */
-export interface IRuleList {
-
+export interface IRuleList extends ICheckPointedList, IList, IForEach {
+    /**
+     * Свойство возвращает правило по его ИД.
+     *
+     * @param id целочисленный идентификатор правила
+     */
+    Rules(id: number): IRule;
+    /**
+     * Метод устанавливает значение свойства Enabled в False у всех правил, которые содержатся в списке.
+     */
+    DisableAll(): void;
+    /**
+     * Метод устанавливает значение свойства Enabled в True у всех правил, которые содержатся в списке.
+     */
+    EnableAll(): void;
 }
