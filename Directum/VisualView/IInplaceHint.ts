993d765d6c5a4e2fb0c8bb31198d1f9d3d5dfda2
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2017-04-07 21:47:27 +0300

    Описал IInplaceHint.ts

--- Directum/VisualView/IInplaceHint.ts
+++ Directum/VisualView/IInplaceHint.ts
@@ -4,5 +4,8 @@ import {TInplaceHintKind} from "../Data/TInplaceHintKind";
  * Управляющий элемент IInplaceHint предназначен для отображения встроенных подсказок на форме.
  */
 export interface IInplaceHint extends IControl {
+    /**
+     * Свойство предоставляет доступ к типу подсказки.
+     */
     Kind: TInplaceHintKind;
 }
